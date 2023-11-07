2b9066b1bfbd465306e93672f5388a9836eb39ae
Kyle Thengvall <kbthengvall@gmail.com>
2023-11-07 08:29:27 -0700

    Initial Commit

---
+++ Shared/Finance_ClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_ClubApp.swift
+//  Shared
+//
+//  Created by Robin Thengvall on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Finance_ClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
